dea2ece53382a7ee9f6803d25da0e833b216695c
Mario Garrido <mrfhitz@gmail.com>
2015-01-03 14:50:51 +0000

    Adicionar linha A e D a base de conhecimento

---
+++ baseconhecimento.pl
@@ -0,0 +1,52 @@
+/* linha verde(D) */
+linha(crell, stadedefrancesaintdenis).
+linha(stadedefrancesaintdenis, crell).
+
+linha(stadedefrancesaintdenis, leshalles).
+linha(leshalles, stadedefrancesaintdenis).
+
+linha(leshalles, garedelyon).
+linha(garedelyon, leshalles).
+
+linha(garedelyon, maisonsafortalfortville).
+linha(maisonsafortalfortville, garedelyon).
+
+linha(maisonsafortalfortville, levertdemaisons).
+linha(levertdemaisons, maisonsafortalfortville).
+
+linha(levertdemaisons, melun).
+linha(levertdemaisons, malesherbes).
+linha(melun, levertdemaisons).
+linha(malesherbes, levertdemaisons).
+
+/* linha vermelha(A) */
+linha(cergy, ladeferense).
+linha(polssy, ladeferense).
+linha(stgermainenlaye, ladeferense).
+linha(ladeferense, cergy).
+linha(ladeferense, polssy).
+linha(ladeferense, stgermainenlaye).
+
+linha(ladeferense, charlesdegaulleetolle).
+linha(charlesdegaulleetolle, ladeferense).
+
+linha(charlesdegaulleetolle, auber).
+linha(auber, charlesdegaulleetolle).
+
+linha(auber, chateletleshalles).
+linha(chateletleshalles, auber).
+
+linha(chateletleshalles, garedelyon).
+linha(garedelyon, chateletleshalles).
+
+
+linha(garedelyon, natlon).
+linha(natlon, garedelyon).
+
+linha(natlon, vincennes).
+linha(vincennes, natlon).
+
+linha(vincennes, marnelavallee).
+linha(vincennes, bolssystleger).
+linha(marnelavallee, vincennes).
+linha(bolssystleger, vincennes).
