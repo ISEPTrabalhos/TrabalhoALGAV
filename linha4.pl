9b0ee4187e13770e4054a6038104534a8471ea8a
Paulo Oliveira <pajolool1471@gmail.com>
2015-01-03 17:54:51 +0000

    Adicionada linha 4 base de conhecimento

---
+++ linha4.pl
@@ -0,0 +1,83 @@
+/* linha roxa(4) */
+
+linha(portedeclignancourt, simplon).
+linha(simplon, portedeclignancourt).
+
+linha(simplon, marcadetpoissonniers).
+linha(marcadetpoissonniers, simplon).
+
+linha(marcadetpoissonniers, chateaurouge).
+linha(chateaurouge, marcadetpoissonniers).
+
+linha(chateaurouge, barbesrochechouart).
+linha(barbesrochechouart, chateaurouge).
+
+linha(barbesrochechouart, garedunord).
+linha(garedunord, barbesrochechouart).
+
+linha(garedunord, garedelest).
+linha(garedelest, garedunord).
+
+linha(garedelest, chateaudeau).
+linha(chateaudeau, garedelest).
+
+linha(chateaudeau, strasbourgsaintdenis).
+linha(strasbourgsaintdenis, chateaudeau).
+
+linha(strasbourgsaintdenis, reaumursebastopol).
+linha(reaumursebastopol, strasbourgsaintdenis).
+
+linha(reaumursebastopol, etiennemarcel).
+linha(etiennemarcel, reaumursebastopol).
+
+linha(etiennemarcel, leshalles).
+linha(leshalles, etiennemarcel).
+
+linha(leshalles, chatelet).
+linha(chatelet, leshalles).
+
+linha(chatelet, cite).
+linha(cite, chatelet).
+
+linha(cite, stmichel).
+linha(stmichel, cite).
+
+linha(stmichel, odeon).
+linha(odeon, stmichel).
+
+linha(odeon, saintgermaindespres).
+linha(saintgermaindespres, odeon).
+
+linha(saintgermaindespres, saintsulpice).
+linha(saintsulpice, saintgermaindespres).
+
+linha(saintsulpice, stplacide).
+linha(stplacide, saintsulpice).
+
+linha(stplacide, montparnassebienvenue).
+linha(montparnassebienvenue, stplacide).
+
+linha(montparnassebienvenue, vavin).
+linha(vavin, montparnassebienvenue).
+
+linha(vavin, raspail).
+linha(raspail, vavin).
+
+linha(raspail, denfertrochereau).
+linha(denfertrochereau, raspail).
+
+linha(denfertrochereau, moutonduvernet).
+linha(moutonduvernet, denfertrochereau).
+
+linha(moutonduvernet, alesia).
+linha(alesia, moutonduvernet).
+
+linha(alesia, portedorleans).
+linha(portedorleans, alesia).
+
+linha(portedorleans, mairiedemontrouge).
+linha(mairiedemontrouge, portedorleans).
+
+
+
+
