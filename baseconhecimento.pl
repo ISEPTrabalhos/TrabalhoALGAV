46d617d764352889969e2e84fe34504680670f24
Mario Garrido <mrfhitz@gmail.com>
2015-01-03 15:12:47 +0000

    adicionar linha A

--- baseconhecimento.pl
+++ baseconhecimento.pl
@@ -50,3 +50,59 @@ linha(vincennes, marnelavallee).
 linha(vincennes, bolssystleger).
 linha(marnelavallee, vincennes).
 linha(bolssystleger, vincennes).
+
+/* linha azul(B) */
+linha(robinson, bourglareine).
+linha(saintremy, bourglareine).
+linha(bourglareine, robinson).
+linha(bourglareine, saintremy).
+
+linha(bourglareine, bagneux).
+linha(bagneux, bourglareine).
+
+linha(bagneux, arcueilcachan).
+linha(arcueilcachan, bagneux).
+
+linha(arcueilcachan, laplace).
+linha(laplace, arcueilcachan).
+
+linha(laplace, gentilly).
+linha(gentilly, laplace).
+
+linha(gentilly, citeuniversitaire).
+linha(citeuniversitaire, gentilly).
+
+linha(citeuniversitaire, denfertrochereau).
+linha(denfertrochereau, citeuniversitaire).
+
+linha(denfertrochereau, portroyal).
+linha(portroyal, denfertrochereau).
+
+linha(portroyal, luxembourg).
+linha(luxembourg, portroyal).
+
+linha(luxembourg, clunylasorbonne).
+linha(clunylasorbonne, luxembourg).
+
+linha(clunylasorbonne, chateletleshalles).
+linha(chateletleshalles, clunylasorbonne).
+
+linha(chateletleshalles, garedunord).
+linha(garedunord, chateletleshalles).
+
+linha(garedunord, laplainestadedefrance).
+linha(laplainestadedefrance, garedunord).
+
+linha(laplainestadedefrance, lacourneuveaubervilliers).
+linha(lacourneuveaubervilliers, laplainestadedefrance).
+
+linha(lacourneuveaubervilliers, lebourget).
+linha(lebourget, lacourneuveaubervilliers).
+
+linha(lebourget, parcdesexpositions).
+linha(lebourget, mitryclaye).
+linha(parcdesexpositions, lebourget).
+linha(mitryclaye, lebourget).
+
+linha(parcdesexpositions, aeroportcharlesdegaulle).
+linha(aeroportcharlesdegaulle, parcdesexpositions).
