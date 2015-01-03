935786e9958f21afd95179dd40ccd5c7a900b9e0
Paulo Oliveira <pajolool1471@gmail.com>
2015-01-03 17:35:58 +0000

    Adicionada linha 3 base de conhecimento

---
+++ linha3.pl
@@ -0,0 +1,81 @@
+/* linha verde-escuro 3 */
+
+linha(pontdelevalloisbecon, anatolefrance).
+linha(anatolefrance, pontdelevalloisbecon).
+
+linha(anatolefrance, louisemichel).
+linha(louisemichel, anatolefrance).
+
+linha(louisemichel, portedechamperret).
+linha(portedechamperret, louisemichel).
+
+linha(portedechamperret, pereirelevallois).
+linha(pereirelevallois, portedechamperret).
+
+linha(pereirelevallois, wagram).
+linha(wagram, pereirelevallois).
+
+linha(wagram, malessherbes).
+linha(malessherbes, wagram).
+
+linha(malessherbes, villiers).
+linha(villiers, malessherbes).
+
+linha(villiers, europe).
+linha(europe, villiers).
+
+linha(europe, garesaintlazare).
+linha(garesaintlazare, europe).
+
+linha(garesaintlazare, saintlazare).
+linha(saintlazare, garesaintlazare).
+
+linha(saintlazare, havrecaumartin).
+linha(havrecaumartin, saintlazare).
+
+linha(havrecaumartin, opera).
+linha(opera, havrecaumartin).
+
+linha(opera, quatreseptembre).
+linha(quatreseptembre, opera).
+
+linha(quatreseptembre, bourse).
+linha(bourse, quatreseptembre).
+
+linha(bourse, sentier).
+linha(sentier, bourse).
+
+linha(sentier, reaumursebastopol).
+linha(reaumursebastopol, sentier).
+
+linha(reaumursebastopol, artsetmetiers).
+linha(artsetmetiers, reaumursebastopol).
+
+linha(artsetmetiers, temple).
+linha(temple, artsetmetiers).
+
+linha(temple, republique).
+linha(republique, temple).
+
+linha(republique, parmentier).
+linha(parmentier, republique).
+
+linha(parmentier, ruesaintmaur).
+linha(ruesaintmaur, parmentier).
+
+linha(ruesaintmaur, perelachaise).
+linha(perelachaise, ruesaintmaur).
+
+linha(perelachaise, gambetta).
+linha(gambetta, perelachaise).
+
+linha(gambetta, portedebagnolet).
+linha(portedebagnolet, gambetta).
+
+linha(portedebagnolet, galleni).
+linha(galleni, portedebagnolet).
+
+
+
+
+
