fe8ca471a0769dc54b1f1255aa22a5c96c33163c
Paulo Oliveira <pajolool1471@gmail.com>
2015-01-03 18:18:41 +0000

    Linha 6 adicionada base de conhecimento

---
+++ linha6.pl
@@ -0,0 +1,95 @@
+/* linha verde claro(6) */
+
+linha(charlesdegaulleetoille, kleber).
+linha(kleber, charlesdegaulleetoille).
+
+linha(kleber, boissiere).
+linha(boissiere, kleber).
+
+linha(boissiere, trocadero).
+linha(trocadero, boissiere).
+
+linha(trocadero, passy).
+linha(passy, trocadero).
+
+linha(passy, birhakeim).
+linha(birhakeim, passy).
+
+linha(birhakeim, dupleix).
+linha(dupleix, birhakeim).
+
+linha(dupleix, lamottepicquetgrenelle).
+linha(lamottepicquetgrenelle, dupleix).
+
+linha(lamottepicquetgrenelle, cambronne).
+linha(cambronne, lamottepicquetgrenelle).
+
+linha(cambronne, sevreslecourbe).
+linha(sevreslecourbe, cambronne).
+
+linha(sevreslecourbe, pasteur).
+linha(pasteur, sevreslecourbe).
+
+linha(pasteur, garemontparnasse).
+linha(garemontparnasse, pasteur).
+
+linha(garemontparnasse, edgarquinet).
+linha(edgarquinet, garemontparnasse).
+
+linha(edgarquinet, raspail).
+linha(raspail, edgarquinet).
+
+linha(raspail, denfertrocheeau).
+linha(denfertrocheeau, raspail).
+
+linha(denfertrocheeau, saintjacques).
+linha(saintjacques, denfertrocheeau).
+
+linha(saintjacques, glaciere).
+linha(glaciere, saintjacques).
+
+linha(glaciere, corvisart).
+linha(corvisart, glaciere).
+
+linha(corvisart, placeditalle).
+linha(placeditalle, corvisart).
+
+linha(placeditalle, nationale).
+linha(nationale, placeditalle).
+
+linha(nationale, chevaleret).
+linha(chevaleret, nationale).
+
+linha(chevaleret, quaidelagare).
+linha(quaidelagare, chevaleret).
+
+linha(quaidelagare, bercy).
+linha(bercy, quaidelagare).
+
+linha(bercy, dugommier).
+linha(dugommier, bercy).
+
+linha(dugommier, daumesnil).
+linha(daumesnil, dugommier).
+
+linha(daumesnil, belair).
+linha(belair, daumesnil).
+
+linha(belair, picpus).
+linha(picpus, belair).
+
+linha(picpus, natlon).
+linha(natlon, picpus).
+
+
+
+
+
+
+
+
+
+
+
+
+
