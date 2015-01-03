11702d70f97535505dc86bc23a983900045a2eb0
Paulo Oliveira <pajolool1471@gmail.com>
2015-01-03 16:52:04 +0000

    Adicionada linha 2 base de conhecimento

---
+++ linha2.pl
@@ -0,0 +1,73 @@
+/* linha azul(2) */
+
+linha(portedauphine, victorhugo).
+linha(victorhugo, portedauphine).
+
+linha(victorhugo, charlesdegaulleetolle).
+linha(charlesdegaulleetolle, victorhugo).
+
+linha(charlesdegaulleetolle, ternes).
+linha(ternes, charlesdegaulleetolle).
+
+linha(ternes, courcelles).
+linha(courcelles, ternes).
+
+linha(courcelles, monceau).
+linha(monceau, courcelles).
+
+linha(monceau, villiers).
+linha(villiers, monceau).
+
+linha(villiers, rome).
+linha(rome, villiers).
+
+linha(rome, placedeclichy).
+linha(placedeclichy, rome).
+
+linha(placedeclichy, blanche).
+linha(blanche, placedeclichy).
+
+linha(blanche, pigalle).
+linha(pigalle, blanche).
+
+linha(pigalle, anvers).
+linha(anvers, pigalle).
+
+linha(anvers, barbesrochechouart).
+linha(barbesrochechouart, anvers).
+
+linha(barbesrochechouart, lachapelle).
+linha(lachapelle, barbesrochechouart).
+
+linha(lachapelle, stalingrad).
+linha(stalingrad, lachapelle).
+
+linha(stalingrad, jaures).
+linha(jaures, stalingrad).
+
+linha(jaures, colonelfabien).
+linha(colonelfabien, jaures).
+
+linha(colonelfabien, belleville).
+linha(belleville, colonelfabien).
+
+linha(belleville, couronnes).
+linha(couronnes, belleville).
+
+linha(couronnes, menilmontant).
+linha(menilmontant, couronnes).
+
+linha(menilmontant, perelachaise).
+linha(perelachaise, menilmontant).
+
+linha(perelachaise, philippeauguste).
+linha(philippeauguste, perelachaise).
+
+linha(philippeauguste, alexandredumas).
+linha(alexandredumas, philippeauguste).
+
+linha(alexandredumas, avron).
+linha(avron, alexandredumas).
+
+linha(avron, natlon).
+linha(natlon, avron).
