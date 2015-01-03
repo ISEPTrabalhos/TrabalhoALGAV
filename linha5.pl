6d7996901bdccaeb12c278bfbf913d90ba803d8e
Paulo Oliveira <pajolool1471@gmail.com>
2015-01-03 18:07:40 +0000

    Linha 5 adicionada base de conhecimento

---
+++ linha5.pl
@@ -0,0 +1,61 @@
+/* linha laranja(5) */
+
+linha(bobignypablopicasso, bobignypantinraymondqueneau).
+linha(bobignypantinraymondqueneau, bobignypablopicasso).
+
+linha(bobignypantinraymondqueneau, eglisedepantin).
+linha(eglisedepantin, bobignypantinraymondqueneau).
+
+linha(eglisedepantin, hoche).
+linha(hoche, portedepantin).
+
+linha(portedepantin, ourcq).
+linha(ourcq, portedepantin).
+
+linha(ourcq, laumiere).
+linha(laumiere, ourcq).
+
+linha(laumiere, jaures).
+linha(jaures, laumiere).
+
+linha(jaures, stalingrad).
+linha(stalingrad, jaures).
+
+linha(stalingrad, garedunord).
+linha(garedunord, stalingrad).
+
+linha(garedunord, garedlest).
+linha(garedlest, garedunord).
+
+linha(garedlest, jacquesbonsergent).
+linha(jacquesbonsergent, garedlest).
+
+linha(jacquesbonsergent, republique).
+linha(republique, jacquesbonsergent).
+
+linha(republique, oberkampf).
+linha(oberkampf, republique).
+
+linha(oberkampf, richardlenoir).
+linha(richardlenoir, oberkampf).
+
+linha(richardlenoir, breguetsabin).
+linha(breguetsabin, richardlenoir).
+
+linha(breguetsabin, bastille).
+linha(bastille, breguetsabin).
+
+linha(bastille, quaidelarapee).
+linha(quaidelarapee, bastille).
+
+linha(quaidelarapee, garedausterlitz).
+linha(garedausterlitz, quaidelarapee).
+
+linha(garedausterlitz, saintmarcel).
+linha(saintmarcel, garedausterlitz).
+
+linha(saintmarcel, campoformio).
+linha(campoformio, saintmarcel).
+
+linha(campoformio, placeditale).
+linha(placeditale, campoformio).
