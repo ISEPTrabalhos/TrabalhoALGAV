2a81d646dab334652ac86571e951b2a3e5c8e394
Paulo Oliveira <pajolool1471@gmail.com>
2015-01-08 17:19:04 +0000

    criacao metodologia para gerar estacoes

--- main.pl
+++ main.pl
@@ -2,3 +2,10 @@
 
 clear:-format('~c~s~c~s', [0x1b, "[H", 0x1b, "[2J"]).
 
+gerar_estacoes:- findall(L, linha(_,L), LE), gerar_estacoes(LE, Estacoes), gerar_estacoes(Estacoes).
+
+gerar_estacoes([]).
+gerar_estacoes([H|T]):- gerar_estacoes(T), atom_string(H, E), assertz(estacoes(E)).
+
+gerar_estacoes([],[]).
+gerar_estacoes([H|T], LE):- gerar_estacoes(T,LR), append(LR, H, LE).
